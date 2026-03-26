a1a3cf46e00ad700169c436caa2be6a9b3a9f6fb
planet1970 <cantezcan70@gmail.com>
2026-03-26 18:07:56 +0300

    edirnego.com düzeltme

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
-export const API_URL = `${API_BASE_URL}/api/v1`;
+export const API_URL = API_BASE_URL;
 
 export const api = axios.create({
     baseURL: API_URL,
